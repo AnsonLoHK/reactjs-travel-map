bc05c2b55f8457d3741d123d8e3136b543512d3e
Burton <bartonchiao0724@lg-gaming.com>
2022-06-02 18:12:39 +0800

    header 用 material ui 的 style 設定 rwd

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import useStyles from "./styles.js";
+// import { Autocomplete } from "@react-google-maps/api";
+
+// icons
+import SearchIcon from "@material-ui/icons/Search";
+
+// component api
+import { AppBar, Toolbar, Typography, Box, InputBase } from "@material-ui/core";
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" className={classes.title}>
+            Travel Advisor
+          </Typography>
+          <Box display="flex">
+            <Typography variant="h6" className={classes.title}>
+              Explore new places
+            </Typography>
+            {/* <Autocomplete> */}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search..."
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+
+            {/* </Autocomplete> */}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
