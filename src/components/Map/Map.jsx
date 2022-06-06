2693971fcbd7ae10fdd5be68f9b2596f8133fb09
Burton <bartonchiao0724@lg-gaming.com>
2022-06-06 18:54:01 +0800

    移植google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+// import Rating from "@material-ui/lab/Rating";
+
+// import useStyles from "./styles";
+
+const AnyReactComponent = ({ text }) => (
+  <div
+    style={{
+      color: "white",
+      background: "grey",
+      padding: "15px 10px",
+      display: "inline-flex",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "100%",
+      transform: "translate(-50%, -50%)",
+    }}
+  >
+    {text}
+  </div>
+);
 
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
+  // const classes = useStyles();
+  const matches = useMediaQuery("(min-width:600px)");
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
 
-export default Map
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        // bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text={"hello i m here"}
+        />
+      </GoogleMapReact>
+      <Paper elevation={0} />
+      <Typography component="div"></Typography>
+      <span>{`(min-width:600px) matches: ${matches}`}</span>
+    </div>
+  );
+};
+
+export default Map;
---
+++ src/components/Map/styles.js
@@ -0,0 +1,9 @@
+// import { makeStyles, createStyles } from "@material-ui/core/styles";
+
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       backgroundColor: theme.color.red,
+//     },
+//   })
+// );
