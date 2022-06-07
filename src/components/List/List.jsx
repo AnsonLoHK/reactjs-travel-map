0e2f979b2f7aee4a1b158c6f41bc76bf804ef615
Burton <bartonchiao0724@lg-gaming.com>
2022-06-07 19:20:24 +0800

    設定下拉選單

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   CircularProgress,
   Grid,
   Typography,
+  InputLabel,
   MenuItem,
   FormControl,
   Select,
@@ -11,8 +12,29 @@ import {
 import useStyles from "./styles";
 
 const List = () => {
+  const [type, setType] = useState("female");
+
   const classes = useStyles();
-  return <div className={classes.root} />;
+
+  const handleChange = (event) => {
+    setType(event.target.value);
+  };
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotel & Attractions around you
+      </Typography>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={handleChange}>
+          <MenuItem value="restaurant">Restaurant</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default List;
--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -1,7 +1,31 @@
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 
-export default makeStyles(() =>
+export default makeStyles((theme) =>
   createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      marginBottom: "30px",
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    loading: {
+      height: "600px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    container: {
+      padding: "25px",
+    },
+    marginBottom: {
+      marginBottom: "30px",
+    },
+    list: {
+      height: "75vh",
+      overflow: "auto",
+    },
     root: {
       backgroundColor: "",
     },
