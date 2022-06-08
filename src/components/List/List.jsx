c97cec82fc326db5407fde6a8fab6bb079c5fa96
Burton <bartonchiao0724@lg-gaming.com>
2022-06-08 10:54:30 +0800

    rating 下拉選單

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -12,13 +12,10 @@ import {
 import useStyles from "./styles";
 
 const List = () => {
-  const [type, setType] = useState("female");
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
 
   const classes = useStyles();
-
-  const handleChange = (event) => {
-    setType(event.target.value);
-  };
 
   return (
     <div className={classes.container}>
@@ -27,10 +24,22 @@ const List = () => {
       </Typography>
       <FormControl component="fieldset" className={classes.formControl}>
         <InputLabel>Type</InputLabel>
-        <Select value={type} onChange={handleChange}>
+        <Select value={type} onChange={(event) => setType(event.target.value)}>
           <MenuItem value="restaurant">Restaurant</MenuItem>
           <MenuItem value="hotels">Hotels</MenuItem>
           <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select
+          value={rating}
+          onChange={(event) => setRating(event.target.value)}
+        >
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
     </div>
